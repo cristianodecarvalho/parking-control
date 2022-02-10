fe4ed8a0b17e0fc873f37f09ff4613d0317f5e6d
Cristiano de Carvalho <cristianfilho18@gmail.com>
2022-02-10 17:54:06 -0300

    Aplicação controle de estacionamento

---
+++ src/main/java/com/cristiano/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,16 @@
+package com.cristiano.api.parkingcontrol.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cristiano.api.parkingcontrol.models.ParkingSpot;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, UUID>{
+	
+	boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
